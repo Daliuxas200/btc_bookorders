edf3eb8bb5e66886196bc992cfddef3bd2007d95
Dalius Slavickas <slavickasd@gmail.com>
2021-03-16 12:53:45 +0200

    added some error handling, comments

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Well hello there
+If needed change the emit period for data and tweak retry parameters.
+Default emit period 30mins
--- app.js
+++ app.js
@@ -1,66 +1,118 @@
 const websocket = require('websocket-stream');
 const request = require('request');
 const fs = require('fs');
+const moment = require('moment');
 
+//Settings for running
+let emitPeriodMinutes = 30;
+let maxRetry = 10;
+let retryAfter = 5000; // ms
+
+let retryCount = 0;
+
+// important to create writeStream outside so that it wont overwrite file after restart
 let myFile = fs.createWriteStream('data.csv');
-setInterval(()=>{
-    book && myFile.write(JSON.stringify(book)+'\n');
-    console.log('wrote to file');
-},1000*60*30)
 
-let depthReadableStream  = websocket('wss://stream.binance.com:9443/ws/btcusdt@depth@1000ms')
-let book = {};
-let askedForSnapshot = false;
+function start(){
 
-depthReadableStream.on('data', async data => {
-    !askedForSnapshot && getBookSnapshot();
+    let depthReadableStream  = websocket('wss://stream.binance.com:9443/ws/btcusdt@depth@1000ms')
+    let book = {};
+    let askedForSnapshot = false;
 
-    let depth = JSON.parse(data.toString());
-    if(book && ((depth.U <= book.lastUpdateId+1) && (depth.u >= book.lastUpdateId+1) || book.lastUpdateId+1 === depth.U)){
-        for(let askDiff of depth.a){
-            let found = false;
-            book.asks = book.asks.map(a => {
-                if(a[0] === askDiff[0]){
-                    found = true;
-                    return askDiff;
-                } else {
-                    return a;
-                }
-            })
-            if(!found && parseFloat(askDiff[1])!==0) book.asks.push(askDiff)
+    // writing data to file at set intervals
+    let writeIntervalID = setInterval(()=>{
+        book && myFile.write(JSON.stringify(book)+'\n');
+        console.log(`Wrote to file on: ${moment().format()} , Data collection duration: ${moment(book.lastTimestamp).diff(moment(book.firstTimestamp),'minutes')} minutes`);
+    },1000*60*emitPeriodMinutes);
+
+    depthReadableStream.on('data', data => {
+
+        // if succesful data, reset Retries;
+        retryCount = 0;
+
+        // Check if request for base Book snapchot has been sent, if not sent it
+        !askedForSnapshot && getBookSnapshot();
+
+        // Parse depth diff data from stream
+        let depth = JSON.parse(data.toString());
+
+        // Check if a book object has been initiated with data and for data continuity based on first/last update ID in the diffs.
+        if(book && ((depth.U <= book.lastUpdateId+1) && (depth.u >= book.lastUpdateId+1) || book.lastUpdateId+1 === depth.U)){
+
+            // loop the diffs to update/add any entries in the current version our book object
+            for(let askDiff of depth.a){
+                let found = false;
+                book.asks = book.asks.map(a => {
+                    if(a[0] === askDiff[0]){
+                        found = true;
+                        return askDiff;
+                    } else {
+                        return a;
+                    }
+                })
+                if(!found && parseFloat(askDiff[1])!==0) book.asks.push(askDiff)
+            }
+            for(let bidDiff of depth.b){
+                let found = false;
+                book.bids = book.bids.map(b=>{
+                    if(b[0] === bidDiff[0]){
+                        found = true;
+                        return bidDiff;
+                    } else {
+                        return b;
+                    }
+                })
+                if(!found && parseFloat(bidDiff[1])!==0) book.bids.push(bidDiff)
+            }
+            // filter out any values with 0.00 quantity
+            book.bids = book.bids.filter(b=>parseFloat(b[1])!==0);
+            book.asks = book.asks.filter(a=>parseFloat(a[1])!==0);
+
+            // update the book lastUpadteId to match that of the latest Diff, also update the timestamp of latest data entry.
+            book.lastUpdateId = depth.u;
+            book.lastTimestamp = moment();
+
+            // calculating totals for Bids and Asks in BTC, spam console.
+            let totalAsks = book.asks.reduce((a,c)=>parseFloat(c[1])+a,0).toFixed(2);
+            let totalBids = book.bids.reduce((a,c)=>parseFloat(c[1])+a,0).toFixed(2);
+            console.log(`Total Bids:${totalBids}, Total Asks:${totalAsks}`);
+
+        // Else if book exists but our updates have skipped over our books timeline, reset the process
+        } else if(book && depth.U > book.lastUpdateId+1){
+            askedForSnapshot = false;
+            book = {};
         }
-        for(let bidDiff of depth.b){
-            let found = false;
-            book.bids = book.bids.map(b=>{
-                if(b[0] === bidDiff[0]){
-                    found = true;
-                    return bidDiff;
-                } else {
-                    return b;
-                }
-            })
-            if(!found && parseFloat(bidDiff[1])!==0) book.bids.push(bidDiff)
-        }
-        book.lastUpdateId = depth.u;
-        book.bids = book.bids.filter(b=>parseFloat(b[1])!==0);
-        book.asks = book.asks.filter(a=>parseFloat(a[1])!==0);
+        
+    })
 
-        let totalAsks = book.asks.reduce((a,c)=>parseFloat(c[1])+a,0).toFixed(2);
-        let totalBids = book.bids.reduce((a,c)=>parseFloat(c[1])+a,0).toFixed(2);
-        console.log(`Total Bids:${totalBids}, Total Asks:${totalAsks}`);
+    depthReadableStream.on('error', err => {
+        console.log(err.name, err.message);
+        setTimeout(()=>{
+            if(retryCount<maxRetry){
+                depthReadableStream.destroy();
+                retryCount++;
+                clearInterval(writeIntervalID)
+                start();
+            } else {
+                console.log('Max retries. Stopping :(')
+                process.exit();
+            }
+        },retryAfter)
+    })
+
+    // Function for getting the snapshot of the Book 
+    function getBookSnapshot(){
+        askedForSnapshot = true;
+        request('https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=1000',{ json: true }, (err, res, body) => {
+            if (err) { 
+                return console.log(err); 
+            }
+            book = {...body};
+            book.firstTimestamp = moment();
+            book.lastTimestamp = moment();
+        });
     }
-    
-})
 
-function getBookSnapshot(){
-    askedForSnapshot = true;
-    request('https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=1000',{ json: true }, (err, res, body) => {
-        if (err) { 
-            return console.log(err); 
-        }
-        book = {...body};
-    });
 }
 
-
-
+start();
